b6cdcd6b55d0e3e4f9956f4a3ebd7bd3e8fe4827
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-04-03 18:16:50 -0700

    adding variable inputs

--- server.R
+++ server.R
@@ -2,17 +2,20 @@ library(shiny)
 library(ggplot2)
 library(plantecophys)
 
-# r <- Aci(seq(60,2000,length=101), Ca=400)
-
 shinyServer(function(input, output){
     
     photo_out <- reactive({
-        Aci(Ci = seq(10,2000,length=101), PPFD = input$PPFD) 
+        
+        Aci(Ci = seq(10,1200,length=100),
+            # VPD = input$VPD,
+            PPFD = input$PPFD,
+            Vcmax = input$Vcmax,
+            Tleaf = input$Tleaf)
     })
 
     output$ACi_curve <- renderPlot({
         pl2 <- ggplot(data = photo_out(), aes(x = Ci, y = ALEAF))
-        pl2 + geom_line()
+        pl2 + geom_line() + ylim(-3, 25)
     })  
 
 })
--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 shinyUI(fluidPage(
     titlePanel("Photosynthesis Teaching App"),
-    helpText("here is some information to help you"),
+    helpText("Play With the Input Values"),
     
     sidebarLayout(
         sidebarPanel(
@@ -11,13 +11,25 @@ shinyUI(fluidPage(
                 "Choose something to display",
                 c("Photosynthesis","other")),
 
+            # numericInput("VPD",
+            #     label = "Vapor Pressure Deficit:",
+            #     min = 0, max = 10, value = 1.5),
+
             numericInput("PPFD",
                 label = "PPFD Value:",
-                min = 0, max = 2000, value = 100)
+                min = 0, max = 1200, value = 1000),
+
+            numericInput("Vcmax",
+                label = "Vcmax:",
+                min = 0, max = 150, value = 50),
+
+            numericInput("Tleaf",
+                label = "Temperature Leaf:",
+                min = 0, max = 100, value = 25)
         ),
 
         mainPanel(
-            textOutput("text1"),
+            # textOutput("text1"),
             plotOutput("ACi_curve")
         )
     )
